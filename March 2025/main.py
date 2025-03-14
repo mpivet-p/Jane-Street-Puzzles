405149fadff2d307305f3b100534d88230b2aed2
Maxime Pivet-Pierre <mpivet-p@student.42.fr>
2025-03-14 22:44:23 +0800

    Added functions to follow lightrays and to calculate all map

--- March 2025/main.py
+++ March 2025/main.py
@@ -1,33 +1,52 @@
-COLS = 5
-ROWS = 5
+COLS, ROWS = 5, 5
 UP, RIGHT, DOWN, LEFT = (-1, 0), (0, 1), (1, 0), (0, -1)
-mirrors = {
-    (UP, 1): RIGHT, (UP, 2): LEFT,
-    (DOWN, 1): LEFT, (DOWN, 2): RIGHT,
-    (LEFT, 1): DOWN, (LEFT, 2): UP,
-    (RIGHT, 1): UP, (RIGHT, 2): DOWN
-} # 1 = /     2 = \
+SLASH, BACKSLASH = 1, 2 # 1 = /     2 = \
 
-def print_grid(grid):
+mirrors = {
+    (UP, SLASH): RIGHT, (UP, BACKSLASH): LEFT,
+    (DOWN, SLASH): LEFT, (DOWN, BACKSLASH): RIGHT,
+    (LEFT, SLASH): DOWN, (LEFT, BACKSLASH): UP,
+    (RIGHT, SLASH): UP, (RIGHT, BACKSLASH): DOWN
+}
+
+def print_grid(grid: list[list]) -> None:
     for row in range(1, len(grid) - 1):
         print("".join(str(c) for c in grid[row][1:-1]))
 
 def check_laser(grid: list[list], row: int, col: int, dir: tuple[int, int], goal: int) -> tuple[bool, tuple]:
-    # print(f"{row=} {col=} {goal=}")
+    return follow_light_ray(grid, row, col, dir) == goal
+
+# def solve(grid):
+#     pass
+
+def follow_light_ray(grid: list[list], row: int, col: int, dir: tuple[int, int]) -> int:
     count = 0
     while count == 0 or grid[row][col] == 0:
         row += dir[0]
         col += dir[1]
         count += 1
-    # print(f"Exiting while loop: {row=} {col=} {count=} {grid[row][col]=}")
-
 
     if (row > 0 and row < ROWS + 1) and (col > 0 and col < COLS + 1):
-        return check_laser(grid, row, col, mirrors[(dir, grid[row][col])], goal // count)
-    return count == goal
+        return count * follow_light_ray(grid, row, col, mirrors[(dir, grid[row][col])])
+    return count
+
+def calculate_lasers_lengths(grid: list[list]) -> int:
+    sums = [0] * 4
+    for i in range(1, COLS + 1):
+        if grid[0][i] == '.':
+            sums[0] += follow_light_ray(grid, 0, i, DOWN)
+        if grid[ROWS + 1][i] == '.':
+            sums[1] += follow_light_ray(grid, ROWS + 1, i, UP)
+    for i in range(1, ROWS + 1):
+        if grid[i][0] == '.':
+            sums[2] += follow_light_ray(grid, i, 0, RIGHT)
+        if grid[i][COLS + 1] == '.':
+            sums[3] += follow_light_ray(grid, i, COLS + 1, LEFT)
+
+    return sums[0] * sums[1] * sums[2] * sums[3]
 
 def main():
-    grid = [
+    grid: list[list] = [
         ['.'] * (COLS + 2),
         ['.'] + [0] * COLS + ['.'],
         ['.'] + [0] * COLS + ['.'],
@@ -41,14 +60,20 @@ def main():
     grid[6][3] = 36
     grid[2][6] = 75
 
-    grid[2][1] = 1
-    grid[5][1] = 2
-    grid[3][3] = 1
-    grid[1][5] = 2
+    grid[1][2] = BACKSLASH
+    grid[1][5] = BACKSLASH
+    grid[2][1] = SLASH
+    grid[3][3] = SLASH
+    grid[3][5] = SLASH
+    grid[4][4] = SLASH
+    grid[5][1] = BACKSLASH
+
     print_grid(grid)
     print(check_laser(grid, 0, 3, DOWN, grid[0][3]))
     print(check_laser(grid, 2, 6, LEFT, grid[2][6]))
     print(check_laser(grid, 0, 5, DOWN, 1))
+    print(follow_light_ray(grid, 6, 2, UP))
+    print(calculate_lasers_lengths(grid))
 
 # Place all the mirrors for the known lasers and then just follow the distances
 
